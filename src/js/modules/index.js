bfaecdbd0764e3c79d9232c38547d996004ed05f
Wataru Esaki <build.wataru@gmail.com>
2021-08-26 13:59:13 +0900

    improve pug template

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -9,3 +9,9 @@ removeTouchDevicesHover()
 secureAnchorElements()
 
 enableScrollAnimation('#js-header', 300)
+
+/**
+ * hm
+ */
+const $hm = document.getElementById('hm')
+$hm.onclick = $hm.classList.toggle('hm--open')
